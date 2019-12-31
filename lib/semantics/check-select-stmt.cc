7b0735597b7888fedc90357f15e7775e6884ff96
joshisameeran17@gmail.com <joshisameeran17@gmail.com>
2019-12-31 11:43:10 +0530

    Use std::visit wherever necessary

--- lib/semantics/check-select-stmt.cc
+++ lib/semantics/check-select-stmt.cc
@@ -11,6 +11,7 @@
 #include "check-select-stmt.h"
 #include "tools.h"
 #include "../common/Fortran.h"
+#include "../common/idioms.h"
 #include "../parser/message.h"
 #include "../parser/tools.h"
 #include <list>
@@ -29,20 +30,26 @@ void SelectStmtChecker::Leave(
   const auto &selectRankStmtSel{
       std::get<parser::Selector>(selectRankStmt.statement.t)};
   // R1149 select-rank-stmt checks
-  if (std::holds_alternative<parser::Expr>(selectRankStmtSel.u)) {
-    // TODO:
-    // 1.Checks if selector is an expression ex.Rank(a+b)
-    //
-  } else if (const auto *variable{
-                 std::get_if<parser::Variable>(&selectRankStmtSel.u)}) {
-    if (const Symbol * entity{GetLastName(*variable).symbol}) {
-      if (!IsAssumedRankArray(*entity)) {  // C1150
-        context_.Say(variable->GetSource(),
-            "Selector is not an assumed-rank array variable"_err_en_US);
-      }
-    }
-  }
-
+  std::visit(
+      common::visitors{
+          [](const parser::Expr &) {
+            // TODO:1.Checks if selector is an
+            // expression ex.Rank(a+b)
+          },
+          [&](const parser::Variable &variable) {
+            if (const Symbol * entity{GetLastName(variable).symbol}) {
+              if (!IsAssumedRankArray(*entity)) {  // C1150
+                context_.Say(variable.GetSource(),
+                    "Selector is not an assumed-rank array variable"_err_en_US);
+              }
+            }
+          },
+          [](auto &) {
+            common::die("Illegal value of selector in SELECT RANK "
+                        "statement");
+          },
+      },
+      selectRankStmtSel.u);
   // R1150 select-rank-case-stmt checks
   auto &rankCaseList{std::get<std::list<parser::SelectRankConstruct::RankCase>>(
       selectRankConstruct.t)};
@@ -55,58 +62,73 @@ void SelectStmtChecker::Leave(
         std::get<parser::Statement<parser::SelectRankCaseStmt>>(rankCase.t)};
     const auto &rank{
         std::get<parser::SelectRankCaseStmt::Rank>(rankCaseStmt.statement.t)};
-
-    // C1153 At most one RANK DEFAULT select-rank-case-stmt allowed
-    if (std::holds_alternative<parser::Default>(rank.u)) {
-      if (!defaultRankFound) {
-        defaultRankFound = true;
-      } else {
-        context_.Say(rankCaseStmt.source,
-            "Not more than one of the selectors of SELECT RANK statement "
-            "may be default"_err_en_US);
-      }
-    }  // C1153 At most one RANK (*) select-rank-case-stmt allowed
-    else if (std::holds_alternative<parser::Star>(rank.u)) {
-      if (!starRankFound) {
-        starRankFound = true;
-      } else {
-        context_.Say(rankCaseStmt.source,
-            "Not more than one of the selectors of SELECT RANK statement "
-            "may be '*'"_err_en_US);
-      }
-      if (std::holds_alternative<parser::Expr>(selectRankStmtSel.u)) {
-        // TODO:1.Checks if selector is an expression ex.Rank(a+b)
-      } else if (const auto *variable{
-                     std::get_if<parser::Variable>(&selectRankStmtSel.u)}) {
-        // C1155 RANK (*) not allowed if the selector has the ALLOCATABLE or
-        // POINTER attribute
-        if (const Symbol * entity{GetLastName(*variable).symbol}) {
-          if (IsAllocatableOrPointer(*entity)) {
-            context_.Say(variable->GetSource(),
-                "RANK (*) cannot be used when selector is "
-                "POINTER or ALLOCATABLE"_err_en_US);
-          }
-        }
-      }
-    } else if (const auto &init{
-                   std::get_if<parser::ScalarIntConstantExpr>(&rank.u)}) {
-      if (auto val{GetIntValue(*init)}) {
-        if (*val < 0 || *val > common::maxRank) {  // C1151
-          context_.Say(rankCaseStmt.source,
-              "The value of the selector must be "
-              "between zero and %d"_err_en_US,
-              common::maxRank);
-        }
-        auto resultPair{matches.insert(*val)};
-        if (!resultPair.second) {  // C1152
-          context_.Say(rankCaseStmt.source,
-              "Same rank values not allowed more than once"_err_en_US);
-        }
-      }
-    }
-	else{
-		DIE ("Illegal value of selector in SELECT RANK case statement");
-	}
+    std::visit(
+        common::visitors{
+            [&](const parser::Default &) {  // C1153
+              if (!defaultRankFound) {
+                defaultRankFound = true;
+              } else {
+                context_.Say(rankCaseStmt.source,
+                    "Not more than one of the selectors of SELECT RANK "
+                    "statement "
+                    "may be default"_err_en_US);
+              }
+            },
+            [&](const parser::Star &) {  // C1153
+              if (!starRankFound) {
+                starRankFound = true;
+              } else {
+                context_.Say(rankCaseStmt.source,
+                    "Not more than one of the selectors of SELECT RANK "
+                    "statement "
+                    "may be '*'"_err_en_US);
+              }
+              std::visit(
+                  common::visitors{
+                      [](const parser::Expr &) {
+                        // TODO:1.Checks if selector is an
+                        // expression ex.Rank(a+b)
+                      },
+                      [&](const parser::Variable &variable) {
+                        // C1155 RANK (*) not allowed if the selector has the
+                        // ALLOCATABLE or POINTER attribute
+                        if (const Symbol *
+                            entity{GetLastName(variable).symbol}) {
+                          if (IsAllocatableOrPointer(*entity)) {
+                            context_.Say(variable.GetSource(),
+                                "RANK (*) cannot be used when selector is "
+                                "POINTER or ALLOCATABLE"_err_en_US);
+                          }
+                        }
+                      },
+                      [](auto &) {
+                        common::die("Illegal value of selector in SELECT RANK "
+                                    "statement");
+                      },
+                  },
+                  selectRankStmtSel.u);
+            },
+            [&](const parser::ScalarIntConstantExpr &init) {
+              if (auto val{GetIntValue(init)}) {
+                if (*val < 0 || *val > common::maxRank) {  // C1151
+                  context_.Say(rankCaseStmt.source,
+                      "The value of the selector must be "
+                      "between zero and %d"_err_en_US,
+                      common::maxRank);
+                }
+                auto resultPair{matches.insert(*val)};
+                if (!resultPair.second) {  // C1152
+                  context_.Say(rankCaseStmt.source,
+                      "Same rank values not allowed more than once"_err_en_US);
+                }
+              }
+            },
+            [](auto &) {
+              common::die("Illegal value of selector in SELECT RANK case "
+                          "statement");
+            },
+        },
+        rank.u);
   }
 }
 
